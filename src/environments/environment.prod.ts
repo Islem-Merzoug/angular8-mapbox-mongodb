a2c7bc3e6b68437100fd428f6d7b4918ae78f917
Islem-Merzoug <59227914+Islem-Merzoug@users.noreply.github.com>
2020-12-15 16:36:31 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  mapboxkey : 'pk.eyJ1IjoiaXNsZW1tZXJ6b3VnIiwiYSI6ImNrYTE5b3I3NTBqbnQzbHBtY3RpbnU3M2sifQ.8wlZ0tx4bc4Suw9gWuhNjw'
+  mapboxkey : 'key_here'
 
 };
